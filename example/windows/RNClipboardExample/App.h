fc823d63305517f0831db78470b3d12390bd26fa
Bartosz Sosnowski <bartosz@janeasystems.com>
2020-08-27 21:19:06 +0200

    Add react-native-windows to the example app, make sure it builds.

---
+++ example/windows/RNClipboardExample/App.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "App.xaml.g.h"
+
+namespace winrt::RNClipboardExample::implementation
+{
+    struct App : AppT<App>
+    {
+        App() noexcept;
+    };
+} // namespace winrt::RNClipboardExample::implementation
+
+
