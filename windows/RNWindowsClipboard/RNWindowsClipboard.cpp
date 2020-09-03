2681288e7fae0657f3125af275204bea17c7b172
Bartosz Sosnowski <bartosz@janeasystems.com>
2020-09-03 11:39:48 +0200

    Setup proper Clipboard logic.

--- windows/RNWindowsClipboard/RNWindowsClipboard.cpp
+++ windows/RNWindowsClipboard/RNWindowsClipboard.cpp
@@ -1,17 +1,42 @@
 #include "pch.h"
 #include "RNWindowsClipboard.h"
 
-static std::string string_test = "Something";
+using namespace winrt::Windows::ApplicationModel::DataTransfer;
 
+winrt::Windows::Foundation::IAsyncAction GetClipboardData(winrt::Microsoft::ReactNative::ReactPromise<winrt::Microsoft::ReactNative::JSValue> promise) {
+  auto captured_promise = promise;
+  auto dataPackageView = Clipboard::GetContent();
+  if (dataPackageView.Contains(StandardDataFormats::Text())) {
+    auto text = co_await dataPackageView.GetTextAsync();
+    winrt::Microsoft::ReactNative::JSValue value;
+    value = winrt::to_string(text);
+    promise.Resolve(value);
+  }
+  else {
+    promise.Resolve("");
+  }
+}
 
 void RNWindowsClipboard::RNCClipboard::getString(winrt::Microsoft::ReactNative::ReactPromise<winrt::Microsoft::ReactNative::JSValue> promise) noexcept {
-  winrt::Microsoft::ReactNative::JSValue value;
-  value = string_test;
-  promise.Resolve(value);
+  auto asyncOp = GetClipboardData(promise);
+  asyncOp.Completed([promise](auto action, auto status) {
+    if (status == winrt::Windows::Foundation::AsyncStatus::Error) {
+      std::stringstream errorCode;
+      errorCode << "0x" << std::hex << action.ErrorCode() << std::endl;
+      auto error = winrt::Microsoft::ReactNative::ReactError();
+      error.Message = "HRESULT " + errorCode.str() + ": " + std::system_category().message(action.ErrorCode());
+      promise.Reject(error);
+    }
+   });
 }
 
 void RNWindowsClipboard::RNCClipboard::setString(std::string text, winrt::Microsoft::ReactNative::ReactPromise<winrt::Microsoft::ReactNative::JSValue> promise) noexcept {
-  string_test = text;
-  winrt::Microsoft::ReactNative::JSValue value;
-  promise.Resolve(value);
+  auto dataPackage = DataPackage();
+  dataPackage.SetText(winrt::to_hstring(text));
+  if (Clipboard::SetContentWithOptions(dataPackage, nullptr)) {
+    winrt::Microsoft::ReactNative::JSValue value;
+    promise.Resolve(value);
+  } else {
+    promise.Reject(winrt::Microsoft::ReactNative::ReactError());
+  }
 }
--- windows/RNWindowsClipboard/pch.h
+++ windows/RNWindowsClipboard/pch.h
@@ -1,5 +1,7 @@
 ﻿#pragma once
 #include <unknwn.h>
+#include <string>
+#include <sstream>
 #include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.Foundation.Collections.h>
-#include <string>
+#include <winrt/Windows.ApplicationModel.DataTransfer.h>
